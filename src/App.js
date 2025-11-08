92d6f9e61c2b1d97343ee6a67f17454889b10edc
Mythicle <morganhopperen@gmail.com>
2025-11-08 14:46:48 +0100

    Initial commit: add tiu-app project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import FAQ from "./pages/FAQ";
+import Kontaktoss from "./pages/Kontaktoss";
+import Kart from "./pages/Kart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+       <div style={{ padding: "20px" }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/kontaktoss" element={<Kontaktoss />} />
+        <Route path="/Kart" element={<Kart />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
